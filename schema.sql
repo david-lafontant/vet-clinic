cda3aa16d9e42f61ab6f6777b3df6856870d1523
david-lafontant <lafontant.david@gmail.com>
2022-04-05 11:04:33 -0600

    Create 'vet_clinic' database and 'animals' table

--- schema.sql
+++ schema.sql
@@ -1,5 +1,10 @@
 /* Database schema to keep the structure of entire database. */
+CREATE DATABASE vet_clinic;
 
-CREATE TABLE animals (
-    name varchar(100)
-);
+CREATE TABLE animals ( id SERIAL PRIMARY KEY, 
+name varchar(100) NOT NULL,
+date_of_birth DATE NOT NULL,
+escape_attempts INTEGER,
+neutered BOOLEAN,
+weight_kg REAL);
+
