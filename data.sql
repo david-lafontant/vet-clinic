4af39022632b8a50f2354d00240942db5d31ae0e
david-lafontant <lafontant.david@gmail.com>
2022-04-06 16:07:37 -0600

    Add data to the "animals", "owners" and "species" tables

--- data.sql
+++ data.sql
@@ -45,3 +45,21 @@ VALUES
   ('Ditto', '2022-05-14', 4, TRUE, 22);
 
   -- Project 3
+
+INSERT INTO owners (name, age) VALUES ('Sam Smith', 34),
+('Jennifer Orwell', 19),
+('Bob', 45),
+('Melody Pond', 77),
+('Dean Winchester', 14),
+('Jodie Whittaker', 38);
+
+INSERT INTO species (name) VALUES ('Pokemon'),('Digimon');
+
+UPDATE animals SET species_id = 2 WHERE name LIKE '%mon';
+UPDATE animals SET species_id = 1 WHERE name NOT LIKE '%mon';
+
+UPDATE animals SET owner_id = 1 WHERE name = 'Agumon';
+UPDATE animals SET owner_id = 2 WHERE name IN ('Gabumon', 'Pikachu');
+UPDATE animals SET owner_id = 3 WHERE name IN ('Devimon', 'Plantmon');
+UPDATE animals SET owner_id = 4 WHERE name IN ('Charmander', 'Squirtle', 'Blossom');
+UPDATE animals SET owner_id = 5 WHERE name IN ('Angemon','Boarmon');
