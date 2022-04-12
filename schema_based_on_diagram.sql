d30fde0f9bcdab11dd1e797fe59b3534669b85a9
david-lafontant <lafontant.david@gmail.com>
2022-04-12 11:53:11 -0600

    Update schema_based_on_diagram file

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -20,3 +20,30 @@ CREATE TABLE "invoices"(
     "payed_at" TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL,
     "medical_history_id" INTEGER NOT NULL,
     PRIMARY KEY(id)
+);
+
+CREATE TABLE "innvoice_items"(
+    "id" INT GENERATED ALWAYS AS IDENTITY,
+    "unit_price" DECIMAL(8, 2) NOT NULL,
+    "quantity" INTEGER NOT NULL,
+    "total_price" DECIMAL(8, 2) NOT NULL,
+    "invoice_id" INTEGER NOT NULL,
+    "treatment_id" INTEGER NOT NULL,
+    PRIMARY KEY(id)
+);
+
+CREATE TABLE "treatments"(
+    "id" INT GENERATED ALWAYS AS IDENTITY,
+    "type" VARCHAR(255) NOT NULL,
+    "name" VARCHAR(255) NOT NULL,
+    PRIMARY KEY(id)
+);
+
+ALTER TABLE
+    "innvoice_items" ADD CONSTRAINT "innvoice_items_invoice_id_foreign" FOREIGN KEY("invoice_id") REFERENCES "invoices"("id");
+ALTER TABLE
+    "invoices" ADD CONSTRAINT "invoices_medical_history_id_foreign" FOREIGN KEY("medical_history_id") REFERENCES "medical_histories"("id");
+ALTER TABLE
+    "medical_histories" ADD CONSTRAINT "medical_histories_patient_id_foreign" FOREIGN KEY("patient_id") REFERENCES "patients"("id");
+ALTER TABLE
+    "innvoice_items" ADD CONSTRAINT "innvoice_items_treatment_id_foreign" FOREIGN KEY("treatment_id") REFERENCES "treatments"("id");
